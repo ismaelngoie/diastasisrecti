3d38c4166cfb795bc4d4457b9b658607e612d93b
Ismael Ngoie <ismael@ngoie.com>
2026-01-30 00:52:58 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+"use client";
 import OnboardingWrapper from "./onboarding/OnboardingWrapper";
 
 export default function Home() {
-  return <OnboardingWrapper />;
+  // Pass full height to wrapper
+  return (
+    <main className="w-full h-full flex flex-col">
+      <OnboardingWrapper />
+    </main>
+  );
 }
