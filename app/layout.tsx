1f902f256086436f0be5c7edde08e000b08b11b0
Ismael Ngoie <ismael@ngoie.com>
2026-01-30 19:15:22 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -128,7 +128,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                   c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                   t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                   y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "v9s9ullomf");
+              })(window, document, "clarity", "script", "v9s9ullom");
             `,
           }}
         />
